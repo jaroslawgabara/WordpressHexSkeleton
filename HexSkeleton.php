1bc805428ebd282402f2091934cc066e14084d49
Jarek <jaroslawgabara@outlook.com>
2021-08-12 23:37:10 +0200

    first commit

---
+++ HexSkeleton.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Hexagonal Architecture Skeleton
+Plugin URI:  http://link to your plugin homepage
+Description: Describe what your plugin is all about in a few short sentences
+Version:     1.0
+Author:      Your name (Yay! Here comes fame... )
+Author URI:  http://link to your website
+License:     GPL2 etc
+License URI: http://link to your plugin license
+*/
